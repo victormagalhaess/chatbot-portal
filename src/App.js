85002d6e380131e351fda6551f44a3c82104455d
Victor Hugo <victormagalhaes00@outlook.com>
2020-04-05 02:09:13 -0300

    Adição de uma pré-formatação md às mensagens recebidas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
 import { Widget, addResponseMessage } from "react-chat-widget";
+import sendUserDataToChatbot from "./features/chatbotCommunication";
+import { updateMessageEscapeCaracteres } from "./shared/utils";
+
 import "react-chat-widget/lib/styles.css";
-import "./App.css";
-import sendUserDataToChatbot from "./actions/chatbotComunicationAction";
+import "./styles/App.css";
 import profilePic from "./img/pp.jpg";
+
 require("dotenv/config");
 
 class App extends Component {
-  handleNewUserMessage = async message => {
+  handleNewUserMessage = async (message) => {
     const responseChatbot = await sendUserDataToChatbot(message);
-    addResponseMessage(responseChatbot.data.text);
+    const escapedResponseChatbot = updateMessageEscapeCaracteres(responseChatbot.data.text);
+    addResponseMessage(escapedResponseChatbot);
   };
 
   componentDidMount = () => {
-    const initialMessage = "Olá, tudo bem? Sou um bot feito para dar informações sobre meu criador!";
+    const initialMessage = "Olá, tudo bem? \n\n Sou um bot feito para dar informações sobre meu criador!";
     const suggestionMessage = `Para começar, me pergunte "O que eu posso perguntar?"`;
     addResponseMessage(initialMessage);
     addResponseMessage(suggestionMessage);
